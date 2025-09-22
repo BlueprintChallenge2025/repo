276167224fa0ee0cb276dd23c7c85eaf9eb5e377
jumbomuffin101 <ryanrawat@gmail.com>
2025-09-22 17:46:53 -0400

    chore(eslint): migrate to ESLint v9 flat config for lambda

---
+++ services/repo-creator/eslint.config.js
@@ -0,0 +1,14 @@
+﻿// services/repo-creator/eslint.config.js
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      // why: keep console noise low in Lambda code
+      "no-console": "warn"
+    }
+  }
+];
